1320c372fc2829a4bf414403443229dce341bba6
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-04-10 02:36:48 +0200

    Hiding files

--- Windows Virtual Desktop/connection.tf
+++
@@ -1,8 +0,0 @@
-## Configuring the Microsoft Azure Provider
-provider "azurerm" {
-    subscription_id = var.subscription_id
-    client_id       = var.terraform_app_client_id
-    client_secret   = var.terraform_app_client_secret
-    tenant_id       = var.aad_tenant_id
-    features {}
-}
