a45cc375835c1876cb1084e2d0df404953fc7fea
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-11 17:49:13 +0100

    Added service principal information

--- windows-virtual-desktop/current/terraform/backend.tf
+++ windows-virtual-desktop/current/terraform/backend.tf
@@ -4,5 +4,9 @@ terraform {
     storage_account_name = "saprdfrctf01"
     container_name       = "windows-virtual-desktop"
     key                  = "prd-terraform.tfstate"
+    subscription_id = var.subscription_id
+    client_id       = var.terraform_sp["client_id"]
+    client_secret   = var.terraform_sp["client_secret"]
+    tenant_id       = var.aad_tenant_id
   }
 }
