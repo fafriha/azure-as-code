8d94626d1d7d91488e93e81c9bd5434207d36a3f
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-16 16:33:20 +0100

    Removed destroy step in workflow

--- windows-virtual-desktop/current/terraform/variables.tf
+++ windows-virtual-desktop/current/terraform/variables.tf
@@ -66,7 +66,7 @@ variable "wvd_application_group" {
       "location"       = "EastUs"
       "friendly_name"  = "Canary"
       "description"    = "Dedicated to canary deployments."
-      "hostpool_name" = "hp-can-frc-wvd-01"
+      "hostpool_name"  = "hp-can-frc-wvd-01"
       "users"          = ["user@company.com"] #WARNING - Adding users to application groups required User Access Administrator or Owner rights
     },
     ag-prd-frc-wvd-02 = {
@@ -75,7 +75,7 @@ variable "wvd_application_group" {
       "location"       = "EastUs"
       "friendly_name"  = "Office"
       "description"    = "Dedicated to medium workload type (Microsoft Word, CLIs, ...)."
-      "hostpool_name" = "hp-prd-frc-wvd-02"
+      "hostpool_name"  = "hp-prd-frc-wvd-02"
       "users"          = ["user@company.com"] #WARNING - Adding users to application groups required User Access Administrator or Owner rights
     }
   }
@@ -145,8 +145,8 @@ variable "wvd_domain" {
   description = "Please provide the required information about your exsiting Active Directory domain."
   type        = map(string)
   default = {
-    "domain_name"    = "friha.fr"
-    "ou_path" = "OU=EMEA,DC=friha,DC=fr"
+    "domain_name" = "friha.fr"
+    "ou_path"     = "OU=EMEA,DC=friha,DC=fr"
   }
 }
 
