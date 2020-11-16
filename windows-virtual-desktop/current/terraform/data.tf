d21be22b210d5c33d3945f6200d43ba99e180222
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-16 12:55:45 +0100

    Disabled adding users to application groups

--- windows-virtual-desktop/current/terraform/data.tf
+++ windows-virtual-desktop/current/terraform/data.tf
@@ -3,10 +3,10 @@ data "azurerm_client_config" "current" {}
 
 ## Gathering application groups members details
 #### WARNING - Adding users to application groups required User Access Administrator or Owner rights
-data "azuread_user" "wvd_users" {
-  for_each            = toset([for u in local.application_groups : u.user])
-  user_principal_name = each.key
-}
+# data "azuread_user" "wvd_users" {
+#   for_each            = toset([for u in local.application_groups : u.user])
+#   user_principal_name = each.key
+# }
 
 ## Gathering hub resource group details
 data "azurerm_resource_group" "hub_resource_group" {
@@ -19,13 +19,15 @@ data "azurerm_virtual_network" "hub_virtual_network" {
   resource_group_name = var.hub_resources["resource_group_name"]
 }
 
-## Gathering hub route table details
-data "azurerm_route_table" "hub_rt" {
-  name                = var.hub_resources["default_route_table_name"]
-  resource_group_name = var.hub_resources["resource_group_name"]
-}
+# ## Gathering hub route table details
+# #### Enable this block if you have a route table routing all traffic to a hub firewall
+# data "azurerm_route_table" "hub_rt" {
+#   name                = var.hub_resources["default_route_table_name"]
+#   resource_group_name = var.hub_resources["resource_group_name"]
+# }
 
-# Gathering managed image details
+# ## Gathering managed image details
+# #### Enable this if you are using a managed image to create session hosts
 # data "azurerm_image" "wvd" {
 #   name                    = "windows10-img"
 #   resource_group_name     = azurerm_resource_group.wvd.name
