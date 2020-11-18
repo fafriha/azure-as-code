295886a842035d6446609deb78257cfa3b87ede0
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-18 21:36:18 +0100

    Fixed syntax issue

--- virtual-machines/deployment/single-virtual-machine.tf
+++ virtual-machines/deployment/single-virtual-machine.tf
@@ -107,7 +107,10 @@ resource "azurerm_virtual_machine_extension" "contoso" {
 
   settings = <<SETTINGS
     {
-      "script": "${base64encode(templatefile("./Install-Agents.ps1", { FileShare=replace(replace("${azurerm_storage_share.contoso.url}", "https:", ""), "/", "\"), RegistrationToken="Token", LocalAdminName=${var.local_admin_account["username"]}})) }"
+      "script": "${base64encode(templatefile("./Install-Agents.ps1", { 
+                                                FileShare = "${replace(replace("${azurerm_storage_share.contoso.url}", "https:", ""), "/", "\\")}", 
+                                                RegistrationToken = "Token", 
+                                                LocalAdminName = "${var.local_admin_account["username"]}"}))}"
     }
   SETTINGS
 }
