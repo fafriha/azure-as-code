e798213f8a4ca3f5adaa3895d04f38d4ff67bfd9
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-18 21:12:56 +0100

    Added single virtual machine deployment

--- virtual-machines/deployment/single-virtual-machine.tf
+++ virtual-machines/deployment/single-virtual-machine.tf
@@ -109,7 +109,7 @@ resource "azurerm_virtual_machine_extension" "contoso" {
     {
       "script": "${base64encode(templatefile("./Install-Agents.ps1", {
                                                   FileShare='replace(replace("${azurerm_storage_share.contoso.url}", "https:", ""), "/", ""\")',
-                                                  RegistrationToken='Toto',
+                                                  RegistrationToken='Token',
                                                   LocalAdminName='${var.local_admin_account["username"]}'}))}"
     }
   SETTINGS
