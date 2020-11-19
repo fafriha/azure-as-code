7844e84c5171f6882d447e936bee852f6d723b69
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-19 21:56:15 +0100

    Added FSLogix agent deployment step

--- windows-virtual-desktop/current/terraform/backend.tf
+++ windows-virtual-desktop/current/terraform/backend.tf
@@ -1,8 +1,8 @@
 ## Using an Azure Storage account as the Terraform backend
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-prd-frc-tf-01"
-    storage_account_name = "saprdfrctf01"
+    resource_group_name  = "rg-prd-frc-hub-01"
+    storage_account_name = "saprdfrctf001"
     container_name       = "windows-virtual-desktop"
     key                  = "prd-terraform.tfstate"
     subscription_id = ""
