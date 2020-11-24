28557c8e41690f28343eff7776e50687c34764b5
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-24 21:35:20 +0100

    Allowed gateway transit

--- windows-virtual-desktop/current/terraform/network.tf
+++ windows-virtual-desktop/current/terraform/network.tf
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network_peering" "hub_peering" {
   virtual_network_name      = var.hub_resources["virtual_network_name"]
   remote_virtual_network_id = azurerm_virtual_network.wvd_virtual_network.id
   allow_gateway_transit     = "true"
-  allow_gateway_transit     = "true"
+  allow_forwarded_traffic   = "true"
 }
 
 ## Peering between WVD's virtual network and hub's virtual network 
