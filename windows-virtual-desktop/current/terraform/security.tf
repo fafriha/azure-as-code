d5037dec3afaf4000c5c66ef51aa45fb79a3f29d
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-25 01:12:13 +0100

    Added hub virtual network address space as source address prefix in NSG rule

--- windows-virtual-desktop/current/terraform/security.tf
+++ windows-virtual-desktop/current/terraform/security.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_security_rule" "wvd_allow_bastion" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefixes     = [data.azurerm_virtual_network.hub_virtual_network.address_space]
+  source_address_prefixes     = data.azurerm_virtual_network.hub_virtual_network.address_space
   destination_address_prefix  = "VirtualNetwork"
   resource_group_name         = azurerm_resource_group.wvd_resource_group.name
   network_security_group_name = azurerm_network_security_group.wvd_network_security_group.name
