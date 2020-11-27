60b1baedb2363980a58c845868752879a5fc1961
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-27 19:05:08 +0100

    Fixed syntax issue

--- windows-virtual-desktop/current/terraform/security.tf
+++ windows-virtual-desktop/current/terraform/security.tf
@@ -70,7 +70,7 @@ resource "azurerm_role_assignment" "wvd_msi" {
   for_each             = local.msi_roles
   role_definition_name = each.value.role
   scope                = each.value.role != "Contributor" ? azurerm_key_vault.wvd_key_vault.id : azurerm_resource_group.wvd_resource_group.id
-  principal_id         = azurerm_user_assigned_identity.wvd_msi[each.value.name].identity.0.principal_id
+  principal_id         = azurerm_user_assigned_identity.wvd_msi[each.value.name].principal_id
 }
 
 ## Adding currently used Service Principals as Key Vault Secrets Officer
