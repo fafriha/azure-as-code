555b70a093ec4615f0b8db0e44cd4d87a788a2a6
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-27 22:15:54 +0100

    Debugging

--- windows-virtual-desktop/current/terraform/compute.tf
+++ windows-virtual-desktop/current/terraform/compute.tf
@@ -15,7 +15,7 @@ resource "azurerm_windows_virtual_machine" "wvd_hosts" {
 
   identity {
     type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.wvd_msi[replace(replace(each.value.hostpool_name, "hp", "msi"), "\\d+$", "01")].principal_id]
+    identity_ids = [azurerm_user_assigned_identity.wvd_msi[replace(replace(each.value.hostpool_name, "hp", "msi"), "\\d+$", "01")].id]
   }
 
   source_image_reference {
