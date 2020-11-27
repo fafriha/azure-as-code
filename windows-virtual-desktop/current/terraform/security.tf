146fb28dfc527bce4882946cb42db49457958d25
Farouk Friha <26277558+faroukfriha@users.noreply.github.com>
2020-11-27 21:18:09 +0100

    Debugging

--- windows-virtual-desktop/current/terraform/security.tf
+++ windows-virtual-desktop/current/terraform/security.tf
@@ -67,10 +67,10 @@ resource "azurerm_user_assigned_identity" "wvd_msi" {
 
 ## Adding Managed Identity as Contributor and Key Vault Secrets Officer
 resource "azurerm_role_assignment" "wvd_msi" {
-  for_each             = { for msi in local.msi_roles : msi.name => msi... }
-  role_definition_name = each.value.name
-  scope                = each.value.name != "Contributor" ? azurerm_key_vault.wvd_key_vault.id : azurerm_resource_group.wvd_resource_group.id
-  principal_id         = azurerm_user_assigned_identity.wvd_msi[each.key].id
+  for_each             = { for msi in local.msi_roles : msi.role => msi... }
+  role_definition_name = each.key
+  scope                = each.key != "Contributor" ? azurerm_key_vault.wvd_key_vault.id : azurerm_resource_group.wvd_resource_group.id
+  principal_id         = azurerm_user_assigned_identity.wvd_msi[each.value.name].id
 }
 
 ## Adding users to application groups
@@ -80,6 +80,10 @@ resource "azurerm_role_assignment" "wvd_users" {
   scope                = azurerm_virtual_desktop_application_group.wvd_application_group[local.application_groups[count.index].name].id
   role_definition_name = "Desktop virtualization user"
   principal_id         = data.azuread_user.wvd_users[local.application_groups[count.index].user].id
+}
+
+output msis{
+  value = azurerm_user_assigned_identity.wvd_msi.indentity[0]
 }
 
 ## Adding currently used Service Principals as Key Vault Secrets Officer
